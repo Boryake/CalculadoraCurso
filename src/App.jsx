ea9528186e4d1caf0092d1d7e3cb90efd85830e0
Boryake <facundopasqua@gmail.com>
2022-04-24 18:18:57 -0300

    Segundo Commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,21 @@
 
 
     const App = () => {
+        const clickHandlerFunction = (text)=>{console.log(text)}
         console.log("render app")
         return (<main className="try-one">
             <Result />
             <div className="numbers">
-                <Button text="1"/>
-                <button>2</button>
-                <button>2</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>0</button>
+                <Button text="1" clickHandler={clickHandlerFunction}/>
+                <Button text="2" clickHandler={clickHandlerFunction}/>
+                <Button text="3" clickHandler={clickHandlerFunction}/>
+                <Button text="4" clickHandler={clickHandlerFunction}/>
+                <Button text="5" clickHandler={clickHandlerFunction}/>
+                <Button text="6" clickHandler={clickHandlerFunction}/>
+                <Button text="7" clickHandler={clickHandlerFunction}/>
+                <Button text="8" clickHandler={clickHandlerFunction}/>
+                <Button text="9" clickHandler={clickHandlerFunction}/>
+                <Button text="0" clickHandler={clickHandlerFunction}/>  
             </div>
             <div className="functions"><button>Clear</button>
             <button>Remove</button>
--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,16 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-const Result = ({value})=> {
-    
-    
-    console.log ("render result", value) 
-    return (
+const Result = ({value})=>  (
         <div className="result">
             {value}
         </div>
     )
-}
 
 Result.propTypes = {
     value: PropTypes.string.isRequired
